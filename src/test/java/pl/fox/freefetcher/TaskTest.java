8cd24ee64cda8a3f1f275cc3fdb5203d9630087c
Chottek <49697145+Chottek@users.noreply.github.com>
2021-05-18 22:30:09 +0200

    Added some more unit tests

--- src/test/java/pl/fox/freefetcher/TaskTest.java
+++ src/test/java/pl/fox/freefetcher/TaskTest.java
@@ -31,6 +31,25 @@ public class TaskTest {
     }
 
     @Test
+    public void testWrongPolaritiesCounter(){
+        List<Quote> quotes = IntStream.range(-5, 5).mapToObj(i -> new Quote(i, "null", "Polarity here is " + i)).collect(Collectors.toList());
+        assertNotEquals(2, Controller.countPolarityTypes(quotes)[0]);
+    }
+
+    @Test
+    public void testKanyeFetching(){
+        final String KANYE_URL = new APIBuilder().withHTTPS().withSite("api.kanye.rest").getFinalURL();
+        List<Quote> quotes = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            String quote = RequestHandler.fetch(KANYE_URL).get("quote").toString();
+            if(quote.length() > 0){
+                quotes.add(new Quote(0, "I don't know", quote));
+            }
+        }
+        assertEquals(5, quotes.size());
+    }
+
+    @Test
     public void testKeySwapper(){
         final String key = "Good";
         final String newKey = "Bad";
@@ -38,5 +57,7 @@ public class TaskTest {
 
         assertTrue(Parser.swapKeys(object, key, newKey).keySet().iterator().next().equals(newKey));
     }
+
+
 
 }
